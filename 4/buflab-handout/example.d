46fb318f884fa49d4a9d23a68ac27969c19da72d
maple5233 <326153996@qq.com>
2017-04-17 23:58:51 +0800

    add lab4

---
+++ 4/buflab-handout/example.d
@@ -0,0 +1,11 @@
+
+example.o：     文件格式 elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	a1 c4 a1 04 08       	mov    0x804a1c4,%eax
+   5:	a3 d4 a1 04 08       	mov    %eax,0x804a1d4
+   a:	68 10 8e 04 08       	push   $0x8048e10
+   f:	c3                   	ret    
---
+++ 4/buflab-handout/example.s
@@ -0,0 +1,4 @@
+mov 0x804a1c4, %eax 
+mov %eax, 0x804a1d4
+push $0x08048e10 
+ret             
